89d54f2411a0cd758c8a5e89021251a33212127a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-24 18:59:51 +0900

    ok

---
+++ serve.js
@@ -0,0 +1,10 @@
+import { serve } from 'https://deno.land/std/http/server.ts'
+import { Hono } from 'https://deno.land/x/hono/mod.ts'
+import { serveStatic } from 'https://deno.land/x/hono/middleware.ts'
+
+const app = new Hono()
+
+app.use('/*', serveStatic({ root: './' }))
+app.get('/', serveStatic({ path: './index.html' }))
+
+serve(app.fetch)
